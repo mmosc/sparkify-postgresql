14812d9f5d0524443ede27a861ad93740a8162fa
mmosc <ma.moscati@gmail.com>
2020-05-26 13:50:36 +0000

    create all tables but songplays

--- sql_queries.py
+++ sql_queries.py
@@ -9,29 +9,33 @@ time_table_drop = "DROP table IF EXISTS time_table"
 # CREATE TABLES
 
 songplay_table_create = ("""
-CREATE TABLE IF NOT EXISTS songplay_table (songplay_id int, start_time numeric, user_id int, level varchar, song_id int, artist_id int, session_id int, location varchar, user_agent varchar);""")
+CREATE TABLE IF NOT EXISTS songplay_table (songplay_id varchar, start_time numeric, user_id int, level varchar, song_id varchar, artist_id int, session_id int, location varchar, user_agent varchar);""")
 
-user_table_create = ("""CREATE TABLE IF NOT EXISTS user_table (user_id int, first_name varchar, last_name varchar, gender varchar, level int);""")
+user_table_create = ("""CREATE TABLE IF NOT EXISTS user_table (user_id int, first_name varchar, last_name varchar, gender varchar, level varchar);""")
 
+song_table_create = ("""CREATE TABLE IF NOT EXISTS song_table (song_id varchar, title varchar, artist_id varchar, year int, duration numeric);""")
 
-song_table_create = ("""CREATE TABLE IF NOT EXISTS song_table (song_id int, title varchar, artist_id int, year int, duration numeric);""")
+artist_table_create = ("""CREATE TABLE IF NOT EXISTS artist_table (artist_id varchar, name varchar, location varchar, latitude numeric, longitude numeric);""")
 
-artist_table_create = ("""CREATE TABLE IF NOT EXISTS artist_table (artist_id int, name varchar, location varchar, latitude numeric, longitude numeric);""")
-
-time_table_create = ("""CREATE TABLE IF NOT EXISTS time_table (start_time numeric, hour int, day int, week int, month int, year int, weekday varchar);""")
+time_table_create = ("""CREATE TABLE IF NOT EXISTS time_table (start_time timestamp, hour int, day int, week int, month int, year int, weekday varchar);""")
 
 # INSERT RECORDS
 
-songplay_table_insert = ("""INSERT INTO songplay_table (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent);""")
+songplay_table_insert = ("""INSERT INTO songplay_table (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) \
+VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)""")
 
-user_table_insert = ("""INSERT INTO user_table (user_id, first_name, last_name, gender, level);""")
+user_table_insert = ("""INSERT INTO user_table (user_id, first_name, last_name, gender, level) \
+VALUES(%s,%s,%s,%s,%s)""")
 
-song_table_insert = ("""INSERT INTO song_table (song_id, title, artist_id, year, duration);""")
+song_table_insert = ("""INSERT INTO song_table (song_id, title, artist_id, year, duration) \
+VALUES (%s,%s,%s,%s,%s)""")
 
-artist_table_insert = ("""INSERT INTO artist_table (artist_id, name, location, latitude, longitude);""")
+artist_table_insert = ("""INSERT INTO artist_table (artist_id, name, location, latitude, longitude) \
+VALUES (%s,%s,%s,%s,%s)""")
 
 
-time_table_insert = ("""INSERT INTO time_table (start_time, hour, day, week, month, year, weekday);""")
+time_table_insert = ("""INSERT INTO time_table (start_time, hour, day, week, month, year, weekday) \
+VALUES (%s,%s,%s,%s,%s,%s,%s)""")
 
 # FIND SONGS
 
